6abbbe918a0102bdc61d6a536c81abaf3562ffab
Zakhar Lanetc <lanetszb@gmail.com>
2019-09-23 21:12:23 +1000

    dpnm: removed duplciated variables from class matrix_portrait

--- numerics/matrix_portrait.py
+++ numerics/matrix_portrait.py
@@ -3,7 +3,6 @@ import os
 import configparser
 import numpy as np
 import math
-import pandas as pd
 import copy
 
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -23,57 +22,40 @@ class Matrix_Portrait:
         # Getting properties from corresponding class
 
         s.props = Props(config_file)
-        s.liq_visc = s.props.liq_visc
 
         # Getting network data from corresponding class
 
         s.network_data = Network_Data(config_file)
+
         s.network_data.process_throats()
         s.network_data.process_pores()
+        s.network_data.process_pore_conns()
+        s.network_data.get_all_conns()
 
-        s.throat_radius = s.network_data.throat_radius
-        s.throat_length = s.network_data.throat_length
-
-        s.conn_ind = s.network_data.conn_ind
-
-        s.conn_number = s.network_data.pores['conn_number']
-        s.pore_coords = s.network_data.pore_coords
-
-        s.pore_number = s.network_data.pore_number
-        s.pore_list = s.network_data.pore_list
-
-        s.front_pores = s.network_data.pores['x_coord']
-
-        s.network_data.get_boundary_pores(s.front_pores)
-        s.boundary_pores = s.network_data.boundary_pores
+        s.network_data.get_boundary_pores(s.network_data.pores['x_coord'])
 
         s.matrix_coeff = None
         s.matrix_row = None
         s.matrix_col = None
+        s.matrix_val = None
         s.vector_B = None
-
-        s.network_data.process_pore_conns()
-        s.network_data.get_all_conns()
-
-        s.pore_conns = s.network_data.pore_conns
-        s.all_conns = s.network_data.all_conns
 
     def get_matrix_coeff(s):
 
-        s.matrix_coeff = math.pi * s.throat_radius ** 4 / \
-                         (8 * s.liq_visc * s.throat_length)
+        s.matrix_coeff = math.pi * s.network_data.throat_radius ** 4 / \
+                         (8 * s.props.liq_visc * s.network_data.throat_length)
 
     def get_matrix_row(s):
 
         s.matrix_row = []
 
-        for i in range(s.pore_number):
+        for i in range(s.network_data.pore_number):
             pores_per_row = []
-            for j in range(s.conn_number[i] + 1):
+            for j in range(s.network_data.pores['conn_number'][i] + 1):
                 pores_per_row.append(i)
             s.matrix_row.append(pores_per_row)
 
-        for idx, value in enumerate(s.boundary_pores):
+        for idx, value in enumerate(s.network_data.boundary_pores):
             s.matrix_row[value] = [value]
 
         s.matrix_row = [y for x in s.matrix_row for y in x]
@@ -81,49 +63,46 @@ class Matrix_Portrait:
 
     def get_matrix_col(s):
 
-        s.pore_list = s.pore_list.reshape(s.pore_number, 1)
-        s.pore_list = s.pore_list.tolist()
+        s.network_data.pore_list = np.array(s.network_data.pore_list).tolist()
 
-        s.matrix_col = [a + b for a, b in zip(s.pore_list,
-                                              s.pore_conns)]
+        s.matrix_col = [a + b for a, b in zip(s.network_data.pore_list,
+                                              s.network_data.pore_conns)]
 
-        for idx, value in enumerate(s.boundary_pores):
+        for idx, value in enumerate(s.network_data.boundary_pores):
             s.matrix_col[value] = [value]
 
         s.matrix_col = [y for x in s.matrix_col for y in x]
         s.matrix_col = np.array(s.matrix_col)
 
     def get_matrix_val(s):
-        conn_ind = np.array(s.conn_ind).tolist()
+        conn_ind = np.array(s.network_data.conn_ind).tolist()
 
         matrix_coeff = np.array(s.matrix_coeff)
-        matrix_coeff = matrix_coeff.reshape(len(s.conn_ind), 1)
+        matrix_coeff = matrix_coeff.reshape(len(conn_ind), 1)
         matrix_coeff = matrix_coeff.tolist()
 
         conn_coeff = [[a, b] for a, b in zip(conn_ind, matrix_coeff)]
 
         s.matrix_val = []
         coeff_in_row = []
-        s.all_cond = []
 
-        for i in range(len(s.all_conns)):
-            for j in range(len(s.all_conns[i])):
+        for i in range(len(s.network_data.all_conns)):
+            for j in range(len(s.network_data.all_conns[i])):
                 for k in range(len(conn_ind)):
-                    if s.all_conns[i][j] == conn_coeff[k][0]:
+                    if s.network_data.all_conns[i][j] == conn_coeff[k][0]:
                         coeff_in_row.append(-1 * conn_coeff[k][1][0])
             s.matrix_val.append(list(coeff_in_row))
             coeff_in_row = []
 
-        s.all_cond = copy.deepcopy(s.matrix_val)
 
-        s.matrix_central = []
+        matrix_central = []
         for i in range(len(s.matrix_val)):
-            s.matrix_central.append(-1 * sum(s.matrix_val[i]))
+            matrix_central.append(-1 * sum(s.matrix_val[i]))
 
-        for i in range(s.pore_number):
-            s.matrix_val[i].insert(0, s.matrix_central[i])
+        for i in range(s.network_data.pore_number):
+            s.matrix_val[i].insert(0, matrix_central[i])
 
-        for idx, value in enumerate(s.boundary_pores):
+        for idx, value in enumerate(s.network_data.boundary_pores):
             s.matrix_val[value] = [1]
 
         s.matrix_val = [y for x in s.matrix_val for y in x]
@@ -131,7 +110,7 @@ class Matrix_Portrait:
 
     def get_vector_B(s, P_in, P_out):
 
-        s.vector_B = list([0] * s.pore_number)
+        s.vector_B = list([0] * s.network_data.pore_number)
 
         for idx, value in enumerate(s.network_data.inlet_pores):
             s.vector_B[value] = P_in
@@ -142,10 +121,10 @@ class Matrix_Portrait:
     def get_flow_rate(s, pressure_list):
 
         inlet_conns = []
-        for i in range(len(s.conn_ind)):
+        for i in range(len(s.network_data.conn_ind)):
             for j in range(len(s.network_data.inlet_pores)):
-                if s.network_data.inlet_pores[j] == s.conn_ind[i][0]:
-                    inlet_conns.append(matrix_portrait.conn_ind[i])
+                if s.network_data.inlet_pores[j] == s.network_data.conn_ind[i][0]:
+                    inlet_conns.append(matrix_portrait.network_data.conn_ind[i])
 
         dp = []
 
@@ -174,8 +153,8 @@ if __name__ == '__main__':
     A = csr_matrix((matrix_portrait.matrix_val,
                     (matrix_portrait.matrix_row,
                      matrix_portrait.matrix_col)),
-                   shape=(matrix_portrait.pore_number,
-                          matrix_portrait.pore_number)).toarray()
+                   shape=(matrix_portrait.network_data.pore_number,
+                          matrix_portrait.network_data.pore_number)).toarray()
 
     x = spsolve(A, matrix_portrait.vector_B)
 
@@ -188,5 +167,5 @@ if __name__ == '__main__':
     x_length = x_max - x_min
     area = x_length ** 2
 
-    k = total_flow * matrix_portrait.liq_visc * x_length / (
-                area * (P_in - P_out))
+    k = total_flow * matrix_portrait.props.liq_visc * x_length / (
+            area * (P_in - P_out))
